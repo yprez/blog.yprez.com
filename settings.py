54939611cc694c9104136bdd8b97719ad1151ae1
Yuri Prezument <y@yprez.com>
2012-05-12 18:58:27 +0300

    Added basic settings

---
+++ settings.py
@@ -0,0 +1,28 @@
+AUTHOR = u"Yuri Prezument"
+SITENAME = u"My Awesome Website!"
+SITEURL = '/'
+
+TIMEZONE = 'Etc/UTC'
+
+DEFAULT_LANG = 'en'
+LINKS = (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+)
+
+SOCIAL = (
+    ('Github', 'https://github.com/yprez'),
+    ('Bitbucket', 'https://bitbucket.org/prez'),
+    ('Twitter', 'https://twitter.com/yprez'),
+    ('LinkedIn', 'http://il.linkedin.com/in/yprez'),
+)
+
+DEFAULT_PAGINATION = True
+THEME = 'notmyidea'
+
+OUTPUT_PATH = '.'
+TWITTER_USERNAME = 'yprez'
+#GOOGLE_ANALYTICS = '' #TODO: enable
+
+MENUITEMS = (('test1', 'http://www.google.com/',), )
