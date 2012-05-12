8afc82e9a85c2495f1f75a561fc78ae8e4dcf8c1
Yuri Prezument <y@yprez.com>
2012-05-12 19:21:27 +0300

    Remove links and menuitems

--- settings.py
+++ settings.py
@@ -1,15 +1,10 @@
-AUTHOR = u"Yuri Prezument"
-SITENAME = u"My Awesome Website!"
+AUTHOR = u'Yuri Prezument'
+SITENAME = u'My Awesome Website!'
 SITEURL = '/'
 
 TIMEZONE = 'Etc/UTC'
 
 DEFAULT_LANG = 'en'
-LINKS = (
-    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-    ('Python.org', 'http://python.org'),
-    ('Jinja2', 'http://jinja.pocoo.org'),
-)
 
 SOCIAL = (
     ('Github', 'https://github.com/yprez'),
@@ -24,5 +19,3 @@ THEME = 'notmyidea'
 OUTPUT_PATH = '.'
 TWITTER_USERNAME = 'yprez'
 #GOOGLE_ANALYTICS = '' #TODO: enable
-
-MENUITEMS = (('test1', 'http://www.google.com/',), )
